1b20b15238d30e32526dd0cb3980d5890699e762
hanwenzhang <41406096@2014.swufe.edu.cn>
2019-10-11 08:05:19 +0800

    Update general.lua

--- files/usr/lib/lua/luci/model/cbi/shadowsocksr/general.lua
+++ files/usr/lib/lua/luci/model/cbi/shadowsocksr/general.lua
@@ -23,7 +23,7 @@ end
 
 
 m = Map("ssrr", translate("Shadowsocksr Transparent Proxy"),
-	" .. "状态 - " .. state_msg)
+	translate("A fast secure tunnel proxy that help you get through firewalls on your router").."" .. "状态 - " .. state_msg)
 
 s = m:section(TypedSection, "shadowsocksr", translate("Settings"))
 s.anonymous = true
